91598b57146abed97015843051a8a4c340f97994
piyush25252 <122280060+piyush25252@users.noreply.github.com>
2024-08-16 18:41:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let allh1 = document.getElementsByTagName('h1');
+console.log(allh1[0]);
+console.log(allh1[1]);
+
+let allClasses = document.getElementsByClassName('sam');
+console.log(allClasses);
+
+let allIds = document.getElementById('samy');
+console.log(allIds);
+
+let firsth1 = document.getElementById(h1);
+console.log(firsth1.textContent);
+
+let an = document.querySelector('a');
+an.setAttribute('href','http://instagram.com');
+
+let c = 0;
+// while(c > 4)
